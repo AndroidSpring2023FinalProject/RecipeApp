1b5d9d0c8af0a43e713f36111e7af955d57062f9
SteveMontelongo <montestev15@gmail.com>
2023-04-15 04:36:00 -0500

    Functionality for fridge added.

--- app/src/main/java/com/example/recipeapp/FridgeAdapter.kt
+++ app/src/main/java/com/example/recipeapp/FridgeAdapter.kt
@@ -8,6 +8,8 @@ import android.widget.ImageButton
 import android.widget.TextView
 import android.widget.Toast
 import androidx.recyclerview.widget.RecyclerView
+import kotlinx.coroutines.Dispatchers.IO
+import kotlin.coroutines.coroutineContext
 
 class FridgeAdapter(private val context: Context, private val ingredients: MutableList<Ingredient>): RecyclerView.Adapter<FridgeAdapter.ItemViewHolder>() {
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ItemViewHolder{
@@ -28,6 +30,8 @@ class FridgeAdapter(private val context: Context, private val ingredients: Mutab
             val ingredient = ingredients[adapterPosition]
             val toast = Toast.makeText(context, ingredient.name, Toast.LENGTH_SHORT)
             toast.show()
+            ingredients.remove(ingredient)
+            notifyItemRemoved(adapterPosition)
         }
     }
 
