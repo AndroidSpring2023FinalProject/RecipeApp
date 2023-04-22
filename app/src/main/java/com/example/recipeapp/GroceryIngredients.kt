63515e6e4bac60fe3630fce6695697b1fa3cd0af
SteveMontelongo <montestev15@gmail.com>
2023-04-22 16:21:01 -0500

    Added GroceryIngredients.kt

--- app/src/main/java/com/example/recipeapp/GroceryIngredients.kt
+++ app/src/main/java/com/example/recipeapp/GroceryIngredients.kt
@@ -1,31 +1,31 @@
 package com.example.recipeapp
 
 import android.util.Log
-import java.util.*
+import java.util.ArrayList
 
 /**
- * THIS IS DESIGNED FOR INGREDIENTS IN FRIDGE.
- *
- * FridgeIngredients is a single instance class that can be called anywhere
+ * THIS IS DESIGNED FOR INGREDIENTS IN GROCERYLIST
+ * GroceryIngredients is a single instance class that can be called anywhere
  * in the application and it will retain it's values. This class contains
- * an ArrayList ingredients referencing the ingredients in the fridge.
+ * an ArrayList ingredients referencing the ingredients in the GroceryList.
  * This class contains functions that will help manipulate the data
  * in the fridge.
  */
-object FridgeIngredients{
+object GroceryIngredients {
+
     init{
         Log.d("Test", "Singleton class invoked")
     }
-    var ingredients = ArrayList<Ingredient>()
+    var ingredients = ArrayList<IngredientGrocery>()
 
     /**
-     * This function takes in Ingredient as a parameter and increments
+     * This function takes in IngredientGrocery as a parameter and increments
      * existing ingredients in the ArrayList by the parameter's .quatity value.
      * If the ingredient does not already exist, then the function will add
      * the quantity value of the parameter to the existing ingredient's quantity value.
-     * @param ingredient object of type Ingredient
+     * @param ingredient object of type IngredientGrocery
      */
-    fun addIngredient(ingredient: Ingredient){
+    fun addIngredient(ingredient: IngredientGrocery){
         Log.d("contain", this.ingredients.toString())
         Log.d("adding", ingredient.toString())
         Log.d("size", this.ingredients.size.toString())
@@ -51,7 +51,7 @@ object FridgeIngredients{
     /**
      * Not complete.
      */
-    fun addIngredientWithQuantity(ingredient: Ingredient, quantity: Int){
+    fun addIngredientWithQuantity(ingredient: IngredientGrocery, quantity: Int){
         if(this.ingredients.contains(ingredient)){
             var index = this.ingredients.indexOf(ingredient)
             (this.ingredients.get(index)).quantity = ((this.ingredients.get(index)).quantity.toInt() + quantity).toString()
@@ -64,9 +64,9 @@ object FridgeIngredients{
     /**
      * Adds multiple ingredients. This solution calls forEach to add them individually
      * to this class's existing arrayList.
-     * @param ingredients An arrayList of Ingredient
+     * @param ingredients An arrayList of IngredientGrocery
      */
-    fun addIngredients(ingredients: ArrayList<Ingredient>){
+    fun addIngredients(ingredients: ArrayList<IngredientGrocery>){
         ingredients.forEach{
             addIngredient(it)
         }
@@ -82,7 +82,7 @@ object FridgeIngredients{
     /**
      * Not complete.
      */
-    fun getIngredientIndex(ingredient: Ingredient): Int{
+    fun getIngredientIndex(ingredient: IngredientGrocery): Int{
         if(this.ingredients.contains(ingredient)){
             return this.ingredients.indexOf(ingredient)
         }
@@ -91,13 +91,13 @@ object FridgeIngredients{
     }
 
     /**
-     * This method will take an Ingredient as a parameter and decrement
+     * This method will take an IngredientGrocery as a parameter and decrement
      * the quantity value of the matching Ingredient by 1. This method
      * will check if the quantity reaches 0. If the check returns true,
-     * then the Ingredient will be removed from the arrayList.
+     * then the IngredientGrocery will be removed from the arrayList.
      * @param ingredient Object of type Ingredient
      */
-    fun decrementIngredient(ingredient: Ingredient){
+    fun decrementIngredient(ingredient: IngredientGrocery){
         if(this.ingredients.contains(ingredient)){
             var index = this.ingredients.indexOf(ingredient)
             this.ingredients.get(index).decrementQuantity()
@@ -113,10 +113,10 @@ object FridgeIngredients{
     /**
      * Increments this class's matching ingredient by the parameter's quantity
      * value if it exists.
-     * @param ingredient Object of type Ingredient
-     * @param index Index of the target ingredient in the arraList.
+     * @param ingredient Object of type IngredientGrocery
+     * @param index Index of the target IngredientGrocery in the arraList.
      */
-    fun incrementIngredient(ingredient: Ingredient, index: Int){
+    fun incrementIngredient(ingredient: IngredientGrocery, index: Int){
         this.ingredients.get(index).quantity = (this.ingredients.get(index).quantity.toInt()+ingredient.quantity.toInt()).toString()
     }
 
@@ -125,7 +125,7 @@ object FridgeIngredients{
      * @return Determines true if the ingredient's quantity value reaches 0 or below.
      *          Otherwise, returns false.
      */
-    private fun isQuantityEmpty(ingredient: Ingredient) :Boolean{
+    private fun isQuantityEmpty(ingredient: IngredientGrocery) :Boolean{
         if(ingredient.quantity.toInt() < 1){
             Log.d("Test", "Quantity: " + ingredient.quantity.toInt())
             return true
